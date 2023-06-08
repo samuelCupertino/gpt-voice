46ff82404c3b1ef81e1e1de0828744d512ed8ad6
Samuel Cupertino <sacupertinno2014@gmail.com>
2023-06-07 22:24:57 -0300

    feat(setup): start project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Main } from '~components/main'
+
+const IndexPage = () => {
+  return <Main name="App" />
+}
+
+export default IndexPage
---
+++ src/popup/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Main } from '~components/main'
+
+const IndexPopup = () => {
+  return <Main />
+}
+
+export default IndexPopup
